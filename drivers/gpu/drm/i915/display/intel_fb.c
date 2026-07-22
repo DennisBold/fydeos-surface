f276f7171250d839101c8a9d385947d0988c33aa
dennis <dennis.sbold@gmail.com>
2026-07-22 21:56:05 +0100

    drm/i915: allow ARGB8888 framebuffers past the plane-format check
    Ported from FydeOS kernel-fydeos_for_pc commit 70a7e4d919bc ("fix black
    screen issue on FydeOS for some old intel GPU", yang@fydeos.io, 2020).
    Some older Intel GPUs report plane capabilities that fail
    drm_any_plane_has_format() for ARGB8888 even though the format works,
    which rejected the framebuffer entirely and produced a black screen.
    Ported to intel_fb.c since intel_framebuffer_init() moved there from
    intel_display.c since this patch was originally written.

--- drivers/gpu/drm/i915/display/intel_fb.c
+++ drivers/gpu/drm/i915/display/intel_fb.c
@@ -1996,7 +1996,8 @@ int intel_framebuffer_init(struct intel_framebuffer *intel_fb,
 	ret = -EINVAL;
 	if (!drm_any_plane_has_format(&dev_priv->drm,
 				      mode_cmd->pixel_format,
-				      mode_cmd->modifier[0])) {
+				      mode_cmd->modifier[0]) &&
+	    mode_cmd->pixel_format != DRM_FORMAT_ARGB8888) {
 		drm_dbg_kms(&dev_priv->drm,
 			    "unsupported pixel format %p4cc / modifier 0x%llx\n",
 			    &mode_cmd->pixel_format, mode_cmd->modifier[0]);
