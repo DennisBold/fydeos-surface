a9964684bc9c6caf2f7c3f000d1a33bb6d5de91f
dennis <dennis.sbold@gmail.com>
2026-07-26 02:14:10 +0100

    drivers/gpu/drm/i915: resolve intel_quirks merge, keep both Dell XPS 13 and Advantech UTC124G3 quirks

--- drivers/gpu/drm/i915/display/intel_quirks.h
+++ drivers/gpu/drm/i915/display/intel_quirks.h
@@ -21,6 +21,8 @@ enum intel_quirk_id {
 	QUIRK_NO_PPS_BACKLIGHT_POWER_HOOK,
 	QUIRK_FW_SYNC_LEN,
 	QUIRK_EDP_LIMIT_RATE_HBR2,
+	QUIRK_SKIP_LINK_CHECK,
+	QUIRK_NO_CHICKEN_ON_S0IX_ENTRY,
 };
 
 void intel_init_quirks(struct intel_display *display);
